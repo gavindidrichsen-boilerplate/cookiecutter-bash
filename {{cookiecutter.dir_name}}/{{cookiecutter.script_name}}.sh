4567da8a60399c33b7991c410d532579532d9d8d
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2018-06-22 10:09:33 +0100

    Update {{cookiecutter.script_name}}.sh
    Shift the logging methods above parameter capture so that logging can be done in the parameter capture as well

--- {{cookiecutter.dir_name}}/{{cookiecutter.script_name}}.sh
+++ {{cookiecutter.dir_name}}/{{cookiecutter.script_name}}.sh
@@ -11,6 +11,20 @@ set -o nounset
 #/   --help: Display this help message
 usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
 
+# Setup logging
+readonly LOG_FILE="/tmp/$(basename "$0").log"
+readonly DATE_FORMAT="+%Y-%m-%d_%H:%M:%S.%2N"
+info()    { echo "[$(date ${DATE_FORMAT})] [INFO]    $*" | tee -a "$LOG_FILE" >&2 ; }
+warning() { echo "[$(date ${DATE_FORMAT})] [WARNING] $*" | tee -a "$LOG_FILE" >&2 ; }
+error()   { echo "[$(date ${DATE_FORMAT})] [ERROR]   $*" | tee -a "$LOG_FILE" >&2 ; }
+fatal()   { echo "[$(date ${DATE_FORMAT})] [FATAL]   $*" | tee -a "$LOG_FILE" >&2 ; exit 1 ; }
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename "${__file}" .sh)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
+
 # set flag variables
 PARAMS=""
 while (( "$#" )); do
@@ -47,20 +61,6 @@ eval set -- "$PARAMS"
 # get the first 2 positional arguments
 arg1="${1:-}"
 arg2="${2:-}"
-
-# Set magic variables for current file & dir
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
-__base="$(basename "${__file}" .sh)"
-__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
-
-# Setup logging
-readonly LOG_FILE="/tmp/$(basename "$0").log"
-readonly DATE_FORMAT="+%Y-%m-%d_%H:%M:%S.%2N"
-info()    { echo "[$(date ${DATE_FORMAT})] [INFO]    $*" | tee -a "$LOG_FILE" >&2 ; }
-warning() { echo "[$(date ${DATE_FORMAT})] [WARNING] $*" | tee -a "$LOG_FILE" >&2 ; }
-error()   { echo "[$(date ${DATE_FORMAT})] [ERROR]   $*" | tee -a "$LOG_FILE" >&2 ; }
-fatal()   { echo "[$(date ${DATE_FORMAT})] [FATAL]   $*" | tee -a "$LOG_FILE" >&2 ; exit 1 ; }
 
 # --- Helper scripts end ---
 # Code begins here...
