d62a7229026ae3c502add0842488287858e4779d
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2019-12-06 17:12:29 +0000

    Add missing test runner
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

---
+++ {{cookiecutter.dir_name}}/test/unit_test_runner.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# export $APPLICATION_ROOT so that test files can source the code
+export APPLICATION_ROOT="${__dir}/.."
+
+find ${APPLICATION_ROOT}/test -name "*.bats" -print -exec bats {} \;
